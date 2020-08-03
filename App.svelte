606d8105f02659cf8af97047871671164ba8f463
stowrt <68393880+stowrt@users.noreply.github.com>
2020-08-03 22:35:34 +0530

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,25 @@
+<script>
+  let todos = [];
+  let todo = "";
+  let addTodo = () => {
+    console.log(todo);
+    todos = [...todos, todo];
+    console.log(todos);
+  };
+  let removeTodo = index => {
+    todos = [...todos.slice(0, index), ...todos.slice(index + 1)];
+  };
+  let completeTodo = index => {
+    console.log(todos[index]);
+  };
+</script>
+<main>
+<input bind:value={todo} type="text"/>
+<button on:click={addTodo}>add</button>
+<ul>
+{#each todos as todo, index}
+  <li on:click={()=>completeTodo(index)}>{todo}</li>
+  <button on:click={()=>removeTodo(index)}>X</button>
+{/each}
+</ul>
+</main>
